fa25f7b6b5c9eeaf248f9520ceadf4e9c31982ff
vlad <vladviolentiy@gmail.com>
2023-04-20 22:46:44 +0300

    fix doc in hash validation

--- src/Validation.php
+++ src/Validation.php
@@ -15,7 +15,7 @@ abstract class Validation
         if(!filter_var($value,FILTER_VALIDATE_EMAIL)) throw new ValidationException($errorText);
     }
 
-    /** @phpstan-assert non-empty-string $hash */
+    /** @phpstan-assert non-empty-string $value */
     public static function hash(string $value,int $length=96,string $errorText = "Incorrect hash value"):void{
         if(!preg_match('/^[0-9a-f]{'.$length.'}$/',$value)) throw new ValidationException($errorText);
     }
