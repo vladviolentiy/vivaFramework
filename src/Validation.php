20429878d8042ae708748dcbe7f41a9d6f4bcebf
vlad <vladviolentiy@gmail.com>
2023-04-20 22:34:33 +0300

    Add validation for id and uuid

--- src/Validation.php
+++ src/Validation.php
@@ -6,6 +6,7 @@ use VladViolentiy\VivaFramework\Exceptions\ValidationException;
 
 abstract class Validation
 {
+    /** @phpstan-assert non-empty-string $hash */
     public static function empty(string $value,string $errorText = "String is empty"):void{
         if($value==="") throw new ValidationException($errorText);
     }
@@ -14,7 +15,17 @@ abstract class Validation
         if(!filter_var($value,FILTER_VALIDATE_EMAIL)) throw new ValidationException($errorText);
     }
 
+    /** @phpstan-assert non-empty-string $hash */
     public static function hash(string $value,int $length=96,string $errorText = "Incorrect hash value"):void{
         if(!preg_match('/^[0-9a-f]{'.$length.'}$/',$value)) throw new ValidationException($errorText);
+    }
+
+    /** @phpstan-assert positive-int $item */
+    public function id(int $item):void{
+        if($item<=0) throw new ValidationException();
+    }
+
+    public function uuid(string $uuid,string $errorText = "Incorrect UUID value"):void{
+        if(!preg_match('/^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[1-8][0-9A-Fa-f]{3}-[ABab89][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$/',$uuid)) throw new ValidationException($errorText);
     }
 }
