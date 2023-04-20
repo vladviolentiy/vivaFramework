f03cb7e982db3a3ce6290e9f7d6484f7f6de644e
vlad <vladviolentiy@gmail.com>
2023-04-20 22:18:03 +0300

    add Hash validation

--- src/Validation.php
+++ src/Validation.php
@@ -13,4 +13,8 @@ abstract class Validation
     public static function email(string $value,string $errorText = "Email incorrect"):void{
         if(!filter_var($value,FILTER_VALIDATE_EMAIL)) throw new ValidationException($errorText);
     }
+
+    public static function hash(string $value,int $length=96,string $errorText = "Incorrect hash value"):void{
+        if(!preg_match('/^[0-9a-f]{'.$length.'}$/',$value)) throw new ValidationException($errorText);
+    }
 }
