2c310864fe9525a732c1074cf2577398b5955b61
vlad <vladviolentiy@gmail.com>
2023-05-30 21:23:38 +0300

    Add Array Validator functions

---
+++ src/ArrayValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace VladViolentiy\VivaFramework;
+
+use VladViolentiy\VivaFramework\Exceptions\ValidationException;
+
+class ArrayValidator
+{
+    /**
+     * @param array<mixed> $input
+     * @return void
+     * @throws ValidationException
+     */
+    public static function intList(array $input):void{
+        foreach ($input as $item) {
+            if(!is_int($item)) throw new ValidationException();
+        }
+    }
+
+    /**
+     * @phpstan-assert string[] $input
+     * @param array<mixed> $input
+     * @return void
+     * @throws ValidationException
+     */
+    public static function stringList(array $input):void{
+        foreach ($input as $item) {
+            if(!is_string($item)) throw new ValidationException();
+        }
+    }
+}
---
+++ tests/ArrayValidatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace VladViolentiy\VivaFramework\Tests;
+
+use PHPUnit\Framework\TestCase;
+use VladViolentiy\VivaFramework\ArrayValidator;
+
+class ArrayValidatorTest extends TestCase
+{
+    public function testIsIntList():void{
+        $info = '[1,2,3,4]';
+        $decoded = json_decode($info,true);
+        ArrayValidator::intList($decoded);
+        self::assertTrue(true);
+    }
+    public function testIsStringList():void{
+        $info = '["333","123"]';
+        $decoded = json_decode($info,true);
+        ArrayValidator::stringList($decoded);
+        self::assertTrue(true);
+    }
+}
