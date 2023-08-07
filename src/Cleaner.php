71ce9043e722d531523e2eb13888f1b2ff01d56c
vlad <vladviolentiy@gmail.com>
2023-08-07 11:57:07 +0300

    Update return types for cleaner

--- src/Cleaner.php
+++ src/Cleaner.php
@@ -4,9 +4,15 @@ namespace VladViolentiy\VivaFramework;
 
 abstract class Cleaner
 {
+    /**
+     * @param string $phone
+     * @return numeric-string
+     */
     public static function phoneNumber(string $phone):string{
         /** @var string $phone */
         $phone = filter_var($phone,FILTER_SANITIZE_NUMBER_INT);
-        return str_replace(["+","-"],"",$phone);
+        /** @var numeric-string $result */
+        $result = str_replace(["+","-"],"",$phone);
+        return $result;
     }
 }
