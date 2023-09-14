f1cbd1d515f639792b7e1e90ba7032c90ca75d0c
Vlad Violentiy <vladviolentiy@gmail.com>
2023-09-14 22:27:41 +0300

    add function  getFile in Req

--- src/Req.php
+++ src/Req.php
@@ -2,6 +2,7 @@
 
 namespace VladViolentiy\VivaFramework;
 
+use Symfony\Component\HttpFoundation\FileBag;
 use Symfony\Component\HttpFoundation\Request;
 
 class Req
@@ -13,15 +14,33 @@ class Req
         $this->request = $request;
     }
 
+    /**
+     * @param non-empty-string $param
+     * @return string
+     */
     public function get(string $param):string{
         /** @var string $line */
         $line = $this->request->get($param);
         return trim($line);
     }
 
+    /**
+     * @param non-empty-string $key
+     * @return string
+     */
     public function getServer(string $key):string{
         /** @var string $line */
         $line = $this->request->server->get($key);
         return trim($line);
+    }
+
+    /**
+     * @param non-empty-string $key
+     * @return FileBag|null
+     */
+    public function getFile(string $key):?FileBag{
+        /** @var FileBag|null $file */
+        $file = $this->request->files->get($key);
+        return $file;
     }
 }
