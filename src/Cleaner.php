b53008696e7365628e79f3a44b3589da973f3c6b
Vlad Violentiy <vladviolentiy@gmail.com>
2023-09-14 22:27:05 +0300

    update types on phone cleaner

--- src/Cleaner.php
+++ src/Cleaner.php
@@ -4,9 +4,15 @@ namespace VladViolentiy\VivaFramework;
 
 abstract class Cleaner
 {
+    /**
+     * @param non-empty-string $phone
+     * @return numeric-string
+     */
     public static function phoneNumber(string $phone):string{
         /** @var string $phone */
         $phone = filter_var($phone,FILTER_SANITIZE_NUMBER_INT);
-        return str_replace(["+","-"],"",$phone);
+        /** @var numeric-string $i */
+        $i = str_replace(['+',"-"],"",$phone);
+        return $i;
     }
 }
