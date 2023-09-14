9cf0557c3b1dfc596ca44e9d846c31dad5c55320
Vlad Violentiy <vladviolentiy@gmail.com>
2023-09-14 22:28:52 +0300

    update tests

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -21,4 +21,9 @@ class ValidationTest extends TestCase
         Validation::hash("9a0a82f0c0cf31470d7affede3406cc9aa8410671520b727044eda15b4c25532a9b5cd8aaf9cec4919d76255b6bfb00f");
         $this->assertTrue(true);
     }
+
+    public function testUUID():void{
+        Validation::uuid("2f7bdad5-f4e7-457b-acab-b7eda3913567");
+        $this->assertTrue(true);
+    }
 }
