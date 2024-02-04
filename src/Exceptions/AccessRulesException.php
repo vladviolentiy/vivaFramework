5ddbedf953988a346a4dbe50c9dea01798b94f99
Vlad Violentiy <vladviolentiy@gmail.com>
2024-02-04 22:49:28 +0300

    add return code for some exceptions

--- src/Exceptions/AccessRulesException.php
+++ src/Exceptions/AccessRulesException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class AccessRulesException extends \Exception
 {
-    public function __construct(string $message = "Access rules exception", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = "Access rules exception", int $code = 403, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/Exceptions/NotfoundException.php
+++ src/Exceptions/NotfoundException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class NotfoundException extends \Exception
 {
-    public function __construct(string $message = "Resource not found", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = "Resource not found", int $code = 404, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
