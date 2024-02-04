2f1756e5e11510f67d6adb1ce860f0a9e51762be
Vlad Violentiy <vladviolentiy@gmail.com>
2024-02-04 22:49:09 +0300

    cleanup

--- src/Databases/Migrations/MysqliMigration.php
+++ src/Databases/Migrations/MysqliMigration.php
@@ -3,7 +3,6 @@
 namespace VladViolentiy\VivaFramework\Databases\Migrations;
 
 use VladViolentiy\VivaFramework\Databases\Mysqli;
-use VladViolentiy\VivaFramework\Exceptions\DatabaseException;
 use VladViolentiy\VivaFramework\Exceptions\MigrationException;
 
 class MysqliMigration extends Mysqli implements MigrationsClassInterface
