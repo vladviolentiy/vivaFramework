f1ea4616f02d545a7ed6c75429d015eec1ee0472
Vlad Violentiy <vladviolentiy@gmail.com>
2024-08-05 20:01:59 +0200

    fix work master query

--- src/Databases/MysqliV2.php
+++ src/Databases/MysqliV2.php
@@ -27,6 +27,7 @@ abstract class MysqliV2
      */
     private function openConnection(string $masterIp, string $login, string $password, string $database): void
     {
+        $this->isMaster = true;
         $this->db = new \mysqli($masterIp, $login, $password, $database);
         if ($this->db->errno !== 0) {
             throw new DatabaseException();
