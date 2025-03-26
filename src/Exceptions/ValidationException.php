c8cad04fcdcdb209739b6579b7d173fe2e9aa2ec
Vlad Violentiy <vladviolentiy@gmail.com>
2025-03-26 22:39:53 +0300

    validation exception code

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class ValidationException extends \Exception
 {
-    public function __construct(string $message = 'Validation exception', int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = 'Validation exception', int $code = 400, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
